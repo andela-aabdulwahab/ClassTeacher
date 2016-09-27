96e55b9f472e7e1755a7382bad5ad33e5ac43552
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-27 13:22:52 +0100

    [Feature] Create authentication views

--- ClassTeacher/account/views.py
+++ ClassTeacher/account/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, FormView
+from django.contrib.auth.forms import UserCreationForm
+from account.forms import LoginForm
 # Create your views here.
+
+
+class LoginView(FormView):
+    template_name = "account/login.html"
+    form_class = LoginForm
+    success_url = "/home"
+
+    def form_valid(self, form):
+        pass
+
+
+class RegisterView(CreateView):
+    template_name = "account/signup.html"
+    form_class = UserCreationForm
+    success_url = "/account/signup-success"
