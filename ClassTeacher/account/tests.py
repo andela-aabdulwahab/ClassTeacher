a9c1f50497a81a3dbcf80c1b63bf3fc3632abae9
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-27 13:42:07 +0100

    [Feature] Test user can login/signup

--- ClassTeacher/account/tests.py
+++ ClassTeacher/account/tests.py
@@ -23,3 +23,94 @@ class TestUserAuthentication(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url+'/account/signup')
         body = self.browser.find_element_by_tag_name("body")
         self.assertIn('Register', body.text)
+
+    def test_user_can_login(self):
+        self.browser.get(self.live_server_url+'/account/login')
+
+        # find username
+        username = self.browser.find_element_by_name("username")
+        username.send_keys("malik")
+
+        # find password
+        password = self.browser.find_element_by_name("password")
+        password.send_keys("malikwahab")
+
+        # find submit botton
+        submit = self.browser.find_element_by_name("submit")
+        submit.click()
+
+        body = self.browser.find_element_by_tag_name("body")
+
+        # assert login success_url and user redirected
+        self.assertNotIn("Login", body.text)
+
+    def test_user_invalid_login_fails(self):
+        self.browser.get(self.live_server_url+'/account/login')
+
+        # find username
+        username = self.browser.find_element_by_name("username")
+        username.send_keys("nouser")
+
+        # find password
+        password = self.browser.find_element_by_name("password")
+        password.send_keys("nouser")
+
+        # find submit botton
+        submit = self.browser.find_element_by_name("submit")
+        submit.click()
+
+        body = self.browser.find_element_by_tag_name("body")
+
+        # assert login success_url and user redirected
+        self.assertIn("Login", body.text)
+
+    def test_user_can_signup(self):
+        self.browser.get(self.live_server_url+'/account/signup')
+
+        # find username
+        # find username
+        username = self.browser.find_element_by_name("username")
+        username.send_keys("adeyi")
+
+        # find password1
+        password = self.browser.find_element_by_name("password2")
+        password.send_keys("malikwahab")
+
+        # find password2
+        password = self.browser.find_element_by_name("password2")
+        password.send_keys("malikwahab")
+
+        # find submit botton
+        submit = self.browser.find_element_by_name("submit")
+        submit.click()
+
+        body = self.browser.find_element_by_tag_name("body")
+
+        # assert login success_url and user redirected
+        self.assertIn("Register", body.text)
+
+
+    def test_invalid_signup_fails(self):
+        self.browser.get(self.live_server_url+'/account/signup')
+
+        # find username
+        # find username
+        username = self.browser.find_element_by_name("username")
+        username.send_keys("malik")
+
+        # find password1
+        password = self.browser.find_element_by_name("password2")
+        password.send_keys("malikwahab")
+
+        # find password2
+        password = self.browser.find_element_by_name("password2")
+        password.send_keys("malikwahab")
+
+        # find submit botton
+        submit = self.browser.find_element_by_name("submit")
+        submit.click()
+
+        body = self.browser.find_element_by_tag_name("body")
+
+        # assert login success_url and user redirected
+        self.assertIn("Register", body.text)
