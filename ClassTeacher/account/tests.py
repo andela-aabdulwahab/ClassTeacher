8babef2958cc62883444721f015581bc649679fe
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-09-27 13:25:09 +0100

    [Feature] change the testing webdriver to phantomjs

--- ClassTeacher/account/tests.py
+++ ClassTeacher/account/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 
 # Create your tests here.
 
@@ -8,18 +8,18 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 class TestUserAuthentication(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = WebDriver()
+        self.browser = webdriver.PhantomJS()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_login_page_exist(self):
-        self.browser.get('/account/login')
+        self.browser.get(self.live_server_url+'/account/login')
         body = self.browser.find_element_by_tag_name("body")
         self.assertIn('Login', body.text)
 
     def test_signup_page_exist(self):
-        self.browser.get('/account/signup')
+        self.browser.get(self.live_server_url+'/account/signup')
         body = self.browser.find_element_by_tag_name("body")
         self.assertIn('Register', body.text)
